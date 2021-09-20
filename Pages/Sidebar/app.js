31eff1ffc5fa8f5887c023c61d5ba866a5624bbb
mshuber1981 <michael.huber1981@outlook.com>
2021-09-20 14:31:08 -0500

    Adding Sidbar content/styles/JS

---
+++ Pages/Sidebar/app.js
@@ -0,0 +1,21 @@
+/*
+=============== 
+Variables
+===============
+*/
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+/*
+=============== 
+Event Listeners
+===============
+*/
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
