a48ee81ce14b1e7cd02761dc436ad4bc35bad3f0
mshuber1981 <michael.huber1981@outlook.com>
2021-10-06 13:39:53 -0500

    Adding Modal content/styles/functionality

--- Pages/Modal/app.js
+++ Pages/Modal/app.js
@@ -1,4 +1,21 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+/*
+=============== 
+Variables
+===============
+*/
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+
+/*
+=============== 
+Event Listeners
+===============
+*/
+modalBtn.addEventListener("click", function () {
+  modal.classList.toggle("open-modal");
+});
+
+modalCloseBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
