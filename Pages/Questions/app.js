55696d30cb2fbdb4e848296fe3afb0b648253b16
mshuber1981 <michael.huber1981@outlook.com>
2021-10-12 18:10:38 -0500

    Updating Questions functionality

--- Pages/Questions/app.js
+++ Pages/Questions/app.js
@@ -14,6 +14,11 @@ const questions = document.querySelectorAll(".question");
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
   btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    }); // Close open question
     question.classList.toggle("show-text");
   });
 });
