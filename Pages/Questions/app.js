2ba2e544babb938704320329b65b7b8e0e953080
mshuber1981 <michael.huber1981@outlook.com>
2021-10-12 18:00:45 -0500

    Adding Questions functionality

--- Pages/Questions/app.js
+++ Pages/Questions/app.js
@@ -1,2 +1,19 @@
-//using selectors inside the element
-// traversing the dom
+// DOM Traversal
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// Using selectors inside elements
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
