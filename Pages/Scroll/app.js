28547d701cc250a85036e97dd23ddcf1ad2c47c6
Michael Huber <michael.huber1981@outlook.com>
2021-11-01 09:41:23 -0500

    Adding Date and Nav toggle functionality

--- Pages/Scroll/app.js
+++ Pages/Scroll/app.js
@@ -1,13 +1,32 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+/*
+=============== 
+Variables
+===============
+*/
+const date = document.getElementById("date");
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links2");
 
-// ********** set date ************
+/*
+=============== 
+Event Listeners
+===============
+*/
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
 
-// ********** close links ************
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 
-// ********** fixed navbar ************
-
-// ********** smooth scroll ************
-// select links
+/*
+=============== 
+Script
+===============
+*/
+date.innerHTML = new Date().getFullYear();
