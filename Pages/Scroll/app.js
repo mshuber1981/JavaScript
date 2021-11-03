0a848e9fd20e12d17ef3060a33032dc102e83d31
Michael Huber <michael.huber1981@outlook.com>
2021-11-03 10:11:52 -0500

    Adding smooth scroll functionality

--- Pages/Scroll/app.js
+++ Pages/Scroll/app.js
@@ -4,9 +4,12 @@ Variables
 ===============
 */
 const date = document.getElementById("date");
+const navbar = document.getElementById("nav");
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links2");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+const topLink = document.querySelector(".top-link");
 
 /*
 =============== 
@@ -22,6 +25,50 @@ navToggle.addEventListener("click", function () {
   } else {
     linksContainer.style.height = 0;
   }
+});
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
 });
 
 /*
