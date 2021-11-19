c81885cae3da877eedc7a609e2b723d3165286d8
Michael Huber <michael.huber1981@outlook.com>
2021-11-19 10:12:27 -0600

    Incorporating back to top link into main page

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ Variables
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
+const topLink = document.querySelector(".top-link");
 
 /*
 =============== 
 Event Listeners
 ===============
 */
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 250) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
 toggleBtn.addEventListener("click", function () {
   sidebar.classList.toggle("show-sidebar");
 });
